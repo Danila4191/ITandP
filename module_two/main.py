3f151d3e24fa9ea1e5b63781608594f7b54f0c35
Danila Zinovjev <danilam.zinovjev@yandex.ru>
2025-04-05 16:27:09 +0300

    add functions

--- module_two/main.py
+++ module_two/main.py
@@ -19,33 +19,32 @@ except Exception as e:
     print(f"{error_loading} : {e}")
 
 def check_csv_injection(df):
-    dangerous_chars = ('=','+','-','@')
+    dangerous_chars = ('=','+','-','@',' =', ' +', ' -', ' @', '#', ' #')
     for col in df.select_dtypes(include=['object']).columns:
-        if df[col].astype(str).apply(lambda x: x.startswith(dangerous_chars)).any():
+        if df[col].astype(str).apply(lambda x: x.startswith(dangerous_chars)).any() :
             print(f"{security_err_CSV_injection} {col}!")
+        elif     df[col].astype(str).apply(lambda x: x.count(' ') >= 2).any() :
+             print(f"{security_err_CSV_injection} {col}!")
         else: 
             print(f"{col} {security_ok}")
 
 check_csv_injection(df)
 
 
-def clean_input(value):
-   
 
+def clean_input(value):
     sql_keywords = sql_keywords_kit
-    xss_patterns = [r'<script.*?>.*?</script>',r'javascript:*',r'onerror=*.']
-    
+    xss_patterns = [r'<script.*?>.*?</script>',r'javascript:.*',r'onerror=.*']
     for keyword in sql_keywords:
         if keyword.lower() in value.lower():
             return "[BLOCKED]"
         return value
-    
 df = df.applymap(lambda x:clean_input(str(x)) if isinstance(x,str) else x)
 print(file_filet)
+
+
 def hash_price(price):
     return hashlib.sha256(str(price).encode()).hexdigest()
-
-
 df['Price_Hashed'] = df['Price'].apply(hash_price)
 print(file_hash)
 
@@ -56,11 +55,12 @@ def encrypt_price(price):
     return cipher.encrypt(str(price).encode()).decode()
 
 def decrypt_price(encrypted_price):
-    return  cipher.decrypt(encrypted_price.encode()).decode()
+    return  cipher.decrypt(encrypted_price).decode()
 
 df['Price_Encrypted'] = df['Price'].apply(encrypt_price)
+print('5 расшифрованных значений столбца Price_Encrypted ')
+print(df['Price_Encrypted'].apply(decrypt_price).head(5))
 print(file_crypt)
-
 output_path = './csv/Laptop_price_secured.csv'
 df.to_csv(output_path,index=False)
 print("{file_save}: {output_path}")
